997c3aaae4611a4ac48c3e105a91030b7bcb1da5
Patrick Miner <patrickminer318@gmail.com>
2018-02-14 20:06:14 -0800

    Added temperature, humidity, and pressure graphs for each city looked up

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td><Chart data={temps} color={'red'} /></td>
+        <td><Chart data={humidity} color={'orange'} /></td>
+        <td><Chart data={pressure} color={'green'} /></td>
       </tr>
     )
   }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,8 +4,13 @@ export default function(state = [], action) {
   switch(action.type) {
     case FETCH_WEATHER:
       // In the below, state is an array of arrays, so by destructuring it it accepts action.payload.data as a new array within itself
-      return [ action.payload.data, ...state ];
-      break; // Break is unneccesary here due to the return statement, but I like it as a reminder and good form
+      if (action.payload.data) {
+        return [ action.payload.data, ...state ];
+        break; // Break is unneccesary here due to the return statement, but I like it as a reminder and good form
+      } else {
+        // adding this in case a faulty search
+        return state;
+      }
   }
 
   return state;
